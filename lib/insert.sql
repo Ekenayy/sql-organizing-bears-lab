7724bee2b0868b4bcb60469507b952b29378e4c6
Ekenayy <ekenayy@gmail.com>
2020-11-04 00:40:09 -0500

    7 of 8 tests completed

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"BOO", 15, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 12, "M", "black", "intense", 1),
+(3,"Geoff", 8, "F", "dark brown", "Nice", 1),
+(4,"Sargeant", 19, "M", "Pink", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Maxx", 11, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
